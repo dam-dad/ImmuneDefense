0409596db3d11811a039e71185c325ddbea342a9
Cypher <izher98@gmail.com>
2021-01-27 10:19:36 +0000

    intento de poner fondo al menu

--- src/main/java/dad/immuneDefense/App.java
+++ src/main/java/dad/immuneDefense/App.java
@@ -12,8 +12,8 @@ public class App extends Application {
 		
 		controller=new MainMenuController();
 		
-		Scene scene=new Scene(controller.getView(),720,600);
-		
+		Scene scene=new Scene(controller.getView(),800,600);
+		scene.getStylesheets().addAll(this.getClass().getResource("/Estilos/MainMenu.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Immune Defense");
 	
