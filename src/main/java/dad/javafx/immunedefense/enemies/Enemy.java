67b6ef8b01c70f04ababad24c292f6bfcef4a014
Adrian Jiménez Mendoza <71876625+Adrian1998-java@users.noreply.github.com>
2021-02-06 18:37:24 +0000

    Enemy acabado, corroborar con JJ

--- src/main/java/dad/javafx/immunedefense/enemies/Enemy.java
+++ src/main/java/dad/javafx/immunedefense/enemies/Enemy.java
@@ -2,4 +2,62 @@ package dad.javafx.immunedefense.enemies;
 
 public class Enemy {
 
+	private double positionX, positionY, velocityX, velocityY, width, height;
+	
+	public Enemy(double positionX, double positionY, double velocityX, double velocityY, double width, double height) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.velocityX = velocityX;
+		this.velocityY = velocityY;
+		this.width = width;
+		this.height = height;
+	}
+
+	public double getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(double positionX) {
+		this.positionX = positionX;
+	}
+
+	public double getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(double positionY) {
+		this.positionY = positionY;
+	}
+
+	public double getVelocityX() {
+		return velocityX;
+	}
+
+	public void setVelocityX(double velocityX) {
+		this.velocityX = velocityX;
+	}
+
+	public double getVelocityY() {
+		return velocityY;
+	}
+
+	public void setVelocityY(double velocityY) {
+		this.velocityY = velocityY;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
 }
