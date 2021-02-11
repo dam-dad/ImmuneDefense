c6141e5fef76f34194c480601b982104d6468333
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-02-11 13:56:48 +0000

    algunos cambios

---
+++ src/main/java/dad/javafx/immunedefense/model/Background.java
@@ -0,0 +1,9 @@
+package dad.javafx.immunedefense.model;
+
+public class Background extends Sprite {
+
+	public Background() {
+		super("/mapImages/Terrain.png");
+	}
+
+}
--- src/main/java/dad/javafx/immunedefense/model/Bullet.java
+++ src/main/java/dad/javafx/immunedefense/model/Bullet.java
@@ -2,8 +2,19 @@ package dad.javafx.immunedefense.model;
 
 public class Bullet extends Sprite {
 
+	// torreta que disparó la bala
+	private Turret turret;
+
 	public Bullet() {
 		super("/mapImages/Proyectile.png");
+	}
+
+	public Turret getTurret() {
+		return turret;
+	}
+
+	public void setTurret(Turret turret) {
+		this.turret = turret;
 	}
 
 }
