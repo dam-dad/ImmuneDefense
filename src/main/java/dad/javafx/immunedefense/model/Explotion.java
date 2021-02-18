7a74c3789997b55b0365c15e1c61f87534ee653f
Guille <guillermomaceiras@hotmail.com>
2021-02-18 23:02:29 +0000

    mejora en la animacion de destruccion

--- src/main/java/dad/javafx/immunedefense/model/Explotion.java
+++ src/main/java/dad/javafx/immunedefense/model/Explotion.java
@@ -1,6 +1,6 @@
 package dad.javafx.immunedefense.model;
 
-import javafx.scene.image.Image;
+
 
 public class Explotion extends Sprite {
 
@@ -8,23 +8,16 @@ public class Explotion extends Sprite {
 	private double time = 0;
 	
 	public Explotion() {
-		super("/mapImages/explotion0.png");
+		super("/mapImages/explosion.gif");
 		setVelocityX(0);
 		setVelocityY(0);
 	}
 
 	@Override
 	public void update(double timeDiff) {
+		
 		time += timeDiff;
-		if( time <=0.25) {
-			this.setImage(new Image("/mapImages/explotion1.png"));
-		}
-		
-		
-		if(time>0.25 & time <=0.5) {
-			this.setImage(new Image("/mapImages/expltion2.png"));
-		}
-		
+
 	if(time>0.5) {
 		
 		this.kill();
